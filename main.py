0843f6aa4c6c021bda3ac134d55a7df49d2e3b55
Sabit Ahmed <sabit.a.sirat@gmail.com>
2023-09-28 12:04:13 -0400

    add project_1 code

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import demo
+import project_1
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    demo.read_file()
+    # demo.read_file()
+    project_1.read_csv_file()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
---
+++ project_1.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+import os
+
+data_dir = "data/project_1/formatted_data/"
+
+
+def read_csv_file():
+    files = os.listdir(f"{data_dir}")
+    df = pd.read_csv(f"{data_dir+files[0]}")
+    print(df.describe())
+
+def read_csv_files():
+    files = os.listdir(f"{data_dir}")
+    for j in files:
+        df = pd.read_csv(f"{data_dir+j}")
