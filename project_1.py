9c466f83821c5bce7d2f1b89b86ee9150cf59b15
Sabit Ahmed <sabit.a.sirat@gmail.com>
2023-09-28 12:09:23 -0400

    add df describe code

--- project_1.py
+++ project_1.py
@@ -7,7 +7,7 @@ data_dir = "data/project_1/formatted_data/"
 
 def read_csv_file():
     files = os.listdir(f"{data_dir}")
-    df = pd.read_csv(f"{data_dir+files[0]}")
+    df = pd.read_csv(f"{data_dir+files[0]}", names=['timestamp','x-axis','y-axis','z-axis'])
     print(df.describe())
 
 def read_csv_files():
