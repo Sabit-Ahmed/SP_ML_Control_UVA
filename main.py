626316bcbcb29f44ed5d7452ab6c9bd386c8b700
Sabit Ahmed <sabit.a.sirat@gmail.com>
2023-09-28 11:52:37 -0400

    demo in main

---
+++ main.py
@@ -0,0 +1,7 @@
+import demo
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    demo.read_file()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
