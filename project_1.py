60979e45c9f8d7f7aa6eb7687b570764731fd721
Sabit Ahmed <sabit.a.sirat@gmail.com>
2023-09-29 02:25:39 -0400

    add code for all data features

--- project_1.py
+++ project_1.py
@@ -3,15 +3,28 @@ import numpy as np
 import os
 
 data_dir = "data/project_1/formatted_data/"
-
+feature_dir = "data/project_1/"
 
 def read_csv_file():
     files = os.listdir(f"{data_dir}")
-    df = pd.read_csv(f"{data_dir+files[0]}", names=['timestamp','x-axis','y-axis','z-axis'])
+    df = pd.read_csv(f"{data_dir + files[0]}", names=['timestamp', 'x-axis', 'y-axis', 'z-axis'])
     return df
 
-def df_ops():
-    df = read_csv_file()
+
+def read_csv_files():
+    output = pd.DataFrame()
+    files = os.listdir(f"{data_dir}")
+    for j in files:
+        df = pd.read_csv(f"{data_dir + j}", names=['timestamp', 'x-axis', 'y-axis', 'z-axis'])
+        df2 = df_ops(df)
+        output = pd.concat([output, df2], axis=0)
+    # print(output)
+    save_csv_file(output)
+
+def save_csv_file(df):
+    df.to_csv(feature_dir+'features.csv', index=None)
+
+def df_ops(df):
     # df['timestamp'] = df['timestamp'] - df['timestamp'][0]
     # df['timestamp'] = df['timestamp']/1000
     df['timestamp'] = pd.to_datetime(df['timestamp'])
@@ -22,17 +35,14 @@ def df_ops():
     feature_df = df.groupby(['timestamp'])
     mean_df = feature_df.mean().reset_index()
     std_df = feature_df.std().reset_index()
-    mean_df.columns = ['timestamp', 'x-mean', 'y-mean', 'z-mean']
-    std_df.columns = ['timestamp', 'x-std', 'y-std', 'z-std']
+    mean_df.drop(columns='timestamp', inplace=True)
+    std_df.drop(columns='timestamp', inplace=True)
+    mean_df.columns = ['x-mean', 'y-mean', 'z-mean']
+    std_df.columns = ['x-std', 'y-std', 'z-std']
     output = pd.concat([mean_df, std_df], axis=1)
-    print(output)
-
-def read_csv_files():
-    files = os.listdir(f"{data_dir}")
-    for j in files:
-        df = pd.read_csv(f"{data_dir+j}")
+    # print(output)
+    return output
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    # demo.read_file()
-    df_ops()
+    read_csv_files()
