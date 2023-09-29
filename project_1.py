b310fce2fe29f9d894f41878463789601cded521
Sabit Ahmed <sabit.a.sirat@gmail.com>
2023-09-29 01:55:18 -0400

    segment window with dataframe tricks

--- project_1.py
+++ project_1.py
@@ -8,13 +8,24 @@ data_dir = "data/project_1/formatted_data/"
 def read_csv_file():
     files = os.listdir(f"{data_dir}")
     df = pd.read_csv(f"{data_dir+files[0]}", names=['timestamp','x-axis','y-axis','z-axis'])
-    df['timestamp'] = df['timestamp']/1000
-    print(df.head(10))
-    print(df.describe())
+    return df
 
 def df_ops():
     df = read_csv_file()
-    df['timestamp'] = df['timestamp']
+    # df['timestamp'] = df['timestamp'] - df['timestamp'][0]
+    # df['timestamp'] = df['timestamp']/1000
+    df['timestamp'] = pd.to_datetime(df['timestamp'])
+    df['timestamp'] = pd.to_timedelta(df['timestamp'].dt.time.astype(str)).dt.total_seconds()
+    # print(df.head(10))
+    # print(df.tail(10))
+    # print(df.describe())
+    feature_df = df.groupby(['timestamp'])
+    mean_df = feature_df.mean().reset_index()
+    std_df = feature_df.std().reset_index()
+    mean_df.columns = ['timestamp', 'x-mean', 'y-mean', 'z-mean']
+    std_df.columns = ['timestamp', 'x-std', 'y-std', 'z-std']
+    output = pd.concat([mean_df, std_df], axis=1)
+    print(output)
 
 def read_csv_files():
     files = os.listdir(f"{data_dir}")
@@ -24,4 +35,4 @@ def read_csv_files():
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     # demo.read_file()
-    read_csv_file()
+    df_ops()
