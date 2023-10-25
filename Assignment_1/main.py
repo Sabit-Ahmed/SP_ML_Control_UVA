8e70f4cfcfbc2deb9767127eeaefde292ab03158
Sabit Ahmed <sabit.a.sirat@gmail.com>
2023-10-25 02:05:22 -0400

    modify project

---
+++ Assignment_1/main.py
@@ -0,0 +1,9 @@
+import demo
+import project_1
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    # demo.read_file()
+    project_1.extract_features()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
